6faad9bc94d7cc2e0cc82291d9b78fb68fd92a99
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-06-29 13:51:50 -0400

    setting up react w. webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Navbar from "./Navbar.js";
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <div>Welcome to my-webpack-react-starter</div>
+
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector(".root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,30 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'source-map',
+    devServer:{
+      static: './dist',     
+    },
+    mode:'development',
+    module: {
+      rules: [{
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader'
+              }
+          },
+          {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+          }
+      ]
+  },
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+      extensions: ['.js', '.jsx', '.css']
+  },
   };
