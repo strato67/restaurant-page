7b70d1a0ae45edd037d08b088813e3daaf7840c5
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-07-20 14:20:33 -0400

    working on menu

---
+++ src/Menu/menuItems.js
@@ -0,0 +1,31 @@
+const menuItems = [
+    {
+        "_id":1,
+        "name":"Cheese Pizza",
+        "productType":"pizza",
+        "description":"Covered with Feta, provolone, cheddar, Parmesan-Asiago, and mozzarella cheese finished with a sprinkle of oregano.",
+        "imgSource": require("../imgs/cheese.jpg")
+    },
+    {
+        "_id":2,
+        "name":"Pepperoni Pizza",
+        "productType":"pizza",
+        "description":"Lots and lots of pepperoni topped with an extra layer of cheese.",
+        "imgSource":require("../imgs/pepperoni.jpg")
+    },
+    {
+        "_id":3,
+        "name":"Sausage Pizza",
+        "productType":"pizza",
+        "description":"Savory Italian sausage with extra cheese.",
+        "imgSource":require("../imgs/sausage.jpg")
+    },
+    {
+        "_id":4,
+        "name":"Fizzly",
+        "productType":"beverage",
+        "description":"Possibly a carbonated lemon/lime or citrus flavored drink similar to Mountain Dew...",
+        "imgSource":require("../imgs/fizz.jpg")
+    }
+]
+export default menuItems;
