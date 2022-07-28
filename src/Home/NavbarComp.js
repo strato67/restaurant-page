8edfc09bf0107cef1cc6693ba9e8308d6de25c71
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-07-28 14:03:43 -0400

    fixing navbar routes

--- src/Home/NavbarComp.js
+++ src/Home/NavbarComp.js
@@ -7,16 +7,16 @@ const NavbarComp = ()=>{
     return(
       <Navbar bg="dark" expand="lg" variant="dark" className="py-4 navbar fixed-top">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="#/">
             <img src={Icon} className='icon' height={48}/>
             Pizza Place
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" defaultActiveKey='/'>
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="menu">Menu</Nav.Link>
-              <Nav.Link href="contact">Contact Us</Nav.Link>
+            <Nav className="me-auto" defaultActiveKey='#/'>
+              <Nav.Link href="#/">Home</Nav.Link>
+              <Nav.Link href="#/menu">Menu</Nav.Link>
+              <Nav.Link href="#/contact">Contact Us</Nav.Link>
 
             </Nav>
           </Navbar.Collapse>
